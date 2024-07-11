aaca6bb566170cc9c9d4e92600475de6f076018a
Roman Novitsky <r0man_n@mail.ru>
2024-07-11 17:22:03 +0300

    feat: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "com.bhft"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks{
+    test {
+        useJUnitPlatform()
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
